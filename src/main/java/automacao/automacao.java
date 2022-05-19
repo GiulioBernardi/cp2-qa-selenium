7f2dd1aabe18e7cba92ad3d6b695cf3fb3c02d54
Giulio Bernardi <giulioccbernardi@gmail.com>
2022-05-19 10:50:02 -0300

    Adicionar chromedriver e pesquisa ao site github

--- src/main/java/automacao/automacao.java
+++ src/main/java/automacao/automacao.java
@@ -12,6 +12,14 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class automacao {
 	public static void main(String[] args) {
-		
-    }
+		//Configura a utilização do webdriver
+		System.setProperty("webdriver.chrome.driver", "src\\main\\java\\chromedriver.exe");
+
+		//Instancia do objeto WebDriver
+		WebDriver driver = new ChromeDriver();
+
+		//Pesquisa a URL no parâmetro na barra de busca do navegador
+		driver.get("https://github.com/");
+
+	}
 }
